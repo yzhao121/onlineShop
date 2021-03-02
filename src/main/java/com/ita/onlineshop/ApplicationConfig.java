fc108a99e9ec0f8c75200152e1a6d0f97b2f35ba
yzhao121 <yzhao142857@gmail.com>
2021-03-02 00:23:24 -0500

    v0.0.3
    Frontend Implementation:
    basic web structure, web flow and corresponding resources

--- src/main/java/com/ita/onlineshop/ApplicationConfig.java
+++ src/main/java/com/ita/onlineshop/ApplicationConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 public class ApplicationConfig {
 
     @Bean(name = "sessionFactory")
