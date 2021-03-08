a7ce12b94d1a28dec03205bc76cd54ff8ab27608
yzhao121 <yzhao142857@gmail.com>
2021-03-07 20:50:16 -0500

    v1.0.0
    Implement placing orders by Spring Web Flow:
      Create SalesOrderDao, SalesOrderService and SalesOrderController;
      Update CartDao
    
    Test
    Now the basic features are all implemented.

--- src/main/java/com/ita/onlineshop/dao/CartDao.java
+++ src/main/java/com/ita/onlineshop/dao/CartDao.java
@@ -1,10 +1,14 @@
 package com.ita.onlineshop.dao;
 
 import com.ita.onlineshop.entity.Cart;
+import com.ita.onlineshop.entity.CartItem;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.io.IOException;
+import java.util.List;
 
 @Repository
 public class CartDao {
@@ -21,4 +25,37 @@ public class CartDao {
         }
         return cart;
     }
+
+    public Cart validate(int cartId) throws IOException {
+        Cart cart = getCartById(cartId);
+        if (cart == null || cart.getCartItem().size() == 0) {
+            throw new IOException(cartId + "");
+        }
+        update(cart);
+        return cart;
+    }
+
+    private void update(Cart cart) {
+        double total = getSalesOrderTotal(cart);
+        cart.setTotalPrice(total);
+
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.saveOrUpdate(cart);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private double getSalesOrderTotal(Cart cart) {
+        double total = 0;
+        List<CartItem> cartItems = cart.getCartItem();
+
+        for (CartItem item : cartItems) {
+            total += item.getPrice();
+        }
+        return total;
+    }
+
 }
