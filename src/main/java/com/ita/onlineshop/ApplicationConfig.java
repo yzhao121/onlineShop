245b08bf331993e6453a3666ee12d0ca34abcfb3
yzhao121 <yzhao142857@gmail.com>
2021-03-07 20:58:05 -0500

    v1.0.1

--- src/main/java/com/ita/onlineshop/ApplicationConfig.java
+++ src/main/java/com/ita/onlineshop/ApplicationConfig.java
@@ -6,10 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
 public class ApplicationConfig {
 
     @Bean(name = "sessionFactory")
@@ -25,7 +23,7 @@ public class ApplicationConfig {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("INSTANCE:3306/ecommerce?createDatabaseIfNotExist=true&serverTimezone=UTC");
+        dataSource.setUrl("jdbc:mysql://INSTANCE:3306/ecommerce?createDatabaseIfNotExist=true&serverTimezone=UTC");
         dataSource.setUsername("USERNAME");
         dataSource.setPassword("PASSWORD");
 
@@ -41,4 +39,3 @@ public class ApplicationConfig {
         return hibernateProperties;
     }
 }
-
